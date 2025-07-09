d11789f4dfeaf8f86392611a83b7eb5eeb0114a7
wintun-devop <wintun.hlaing@outlook.com>
2025-07-09 16:37:05 +0630

    add uuid string

--- flask_apis/server/utils/helper/__init__.py
+++ flask_apis/server/utils/helper/__init__.py
@@ -1,4 +1,4 @@
-import uuid
+import uuid,random,string
 
 def to_dict(obj)->dict: 
     return {column.name: getattr(obj, column.name) for column in obj.__table__.columns}
@@ -8,3 +8,9 @@ def to_lower_case_string(input_string:str)->str:
 
 def uuid_string()->str:
     return uuid.uuid4()
+
+def generate_unique_string() -> str:
+    length = 32
+    chars = string.ascii_letters + string.digits
+    result = ''.join(random.choice(chars) for _ in range(length))
+    return result
