74975bfa38bce0d2afa957c20e2c7245f3998bb1
wintun-devop <wintun.hlaing@outlook.com>
2025-07-09 16:28:09 +0630

    update env

--- flask_apis/app.py
+++ flask_apis/app.py
@@ -1,18 +1,24 @@
 from server import create_app,db
-
+#import blue print
+from server.routes.server_test import api_test_bp
+from server.routes.app.product import product_bp
 
 #create app instance
 app = create_app()
+
+#register Blue print
+app.register_blueprint(api_test_bp)
+app.register_blueprint(product_bp)
 
 
 if __name__ == "__main__":
     #add host as 0.0.0.0 to be accessible from outside when running as a #container
     #app.run(host='0.0.0.0')
-    with app.app_context():
-        write_engine = db.get_engine(bind='write')
-        read_engine = db.get_engine(bind='read')
-        with write_engine.connect() as conn:
-            print("✅ Write DB connected:", conn.execute("SELECT 1").scalar())
-        with read_engine.connect() as conn:
-            print("✅ Read DB connected:", conn.execute("SELECT 1").scalar())
-    app.run(debug=True, port=8000, host='0.0.0.0')
+    # with app.app_context():
+    #     write_engine = db.get_engine(bind='write')
+    #     read_engine = db.get_engine(bind='read')
+    #     with write_engine.connect() as conn:
+    #         print("✅ Write DB connected:", conn.execute("SELECT 1").scalar())
+    #     with read_engine.connect() as conn:
+    #         print("✅ Read DB connected:", conn.execute("SELECT 1").scalar())
+    app.run(debug=True)
--- flask_apis/server/services/db_session.py
+++ flask_apis/server/services/db_session.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from server_config import Config
+from server.server_config import Config
 
-write_engine = create_engine(Config.WRITE_DB_URI)
-read_engine = create_engine(Config.READ_DB_URI)
+write_engine = create_engine(Config.SQLALCHEMY_BINDS["write"])
+read_engine = create_engine(Config.SQLALCHEMY_BINDS["read"])
 
 WriteSession = scoped_session(sessionmaker(bind=write_engine))
 ReadSession = scoped_session(sessionmaker(bind=read_engine))
 
+db_read_session = WriteSession()
+db_write_session = ReadSession()
+
 def get_session(operation="read"):
     return WriteSession() if operation == "write" else ReadSession()
+
+
